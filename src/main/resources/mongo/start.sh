96cd143b9b07fb78711e19e49395ad32d9223a29
Lolupo <lolupo@users.noreply.github.com>
2024-11-03 14:53:01 +0100

    build(commission-rate): fix bin ip to allows access outside container, rename database and collection

--- src/main/resources/mongo/start.sh
+++ src/main/resources/mongo/start.sh
@@ -6,7 +6,7 @@ mongod --fork --logpath /var/log/mongodb.log --bind_ip_all
 sleep 5
 
 # Utilisation de mongoimport pour insérer le fichier JSON dans la base de données et la collection
-/usr/bin/mongoimport --db my_database --collection my_collection --file /tmp/mongo-rule.json
+/usr/bin/mongoimport --db malt --collection commission_rate --file /tmp/mongo-rule.json
 
 # Maintien du conteneur actif
 tail -f /dev/null
