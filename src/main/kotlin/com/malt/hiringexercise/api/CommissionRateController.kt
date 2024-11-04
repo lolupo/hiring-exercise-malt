8b39c7618953d6c7557ac254d27b52ea2bba3b30
Lolupo <lolupo@users.noreply.github.com>
2024-11-04 17:16:04 +0100

    feat(commission-rate): create new path to add and more documentation

--- src/main/kotlin/com/malt/hiringexercise/api/CommissionRateController.kt
+++ src/main/kotlin/com/malt/hiringexercise/api/CommissionRateController.kt
@@ -1,17 +1,21 @@
 package com.malt.hiringexercise.api
 
+import com.malt.hiringexercise.domain.model.CommissionRate
+import com.malt.hiringexercise.service.AddCommissionRateService
 import com.malt.hiringexercise.service.IpStackService
 import com.malt.hiringexercise.service.SearchCommissionRate
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/v1/commission-rate")
-class DummyController(
+class CommissionRateController(
     private val searchCommissionRate: SearchCommissionRate,
+    private val addCommissionRateService: AddCommissionRateService,
     private val ipStackService: IpStackService
 ) {
 
@@ -27,11 +31,44 @@ class DummyController(
         searchCriteria: SearchCriteria
     ): Double {
 
-        val clientLocation = ipStackService.getIpDetails(searchCriteria.client.ip)
-        val freelancerLocation = ipStackService.getIpDetails(searchCriteria.freelancer.ip)
+        val clientLocation = ipStackService.execute(searchCriteria.client.ip)
+        val freelancerLocation = ipStackService.execute(searchCriteria.freelancer.ip)
 
         return clientLocation.takeIf { it == freelancerLocation }
-            ?.let { searchCommissionRate.searchRateByCriteria(searchCriteria, it) }
+            ?.let { searchCommissionRate.execute(searchCriteria, it) }
             ?: throw IllegalArgumentException("Client and freelancer are not in the same country")
+    }
+
+    @PostMapping("/add")
+    @Operation(
+        summary = "Add a new Commission rate",
+        responses = [
+            ApiResponse(
+                responseCode = "201",
+                description = "The created Commission rate",
+                content = [Content(
+                    schema = Schema(implementation = CommissionRate::class),
+                    examples = [ExampleObject(
+                        name = "CommissionRateExample",
+                        value = "{\"name\": \"Standard\", \"rate\": 10, \"restrictions\": {\"or\": [{\"mission_duration\": {\"\$gt\": \"2months\"}}, {\"commercial_relationship_duration\": {\"\$gt\": \"2months\"}}], \"country\": \"ES\"}}"
+                    )]
+                )]
+            )
+        ]
+    )
+    fun addCommissionRate(
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = [Content(
+                schema = Schema(implementation = CommissionRate::class),
+                examples = [ExampleObject(
+                    name = "CommissionRateBodyExample",
+                    value = "{\"name\": \"Standard\", \"rate\": 10, \"restrictions\": {\"or\": [{\"mission_duration\": {\"\$gt\": \"2months\"}}, {\"commercial_relationship_duration\": {\"\$gt\": \"2months\"}}], \"country\": \"ES\"}}"
+                )]
+            )]
+        )
+        @RequestBody
+        commissionRate: CommissionRate
+    ): CommissionRate {
+        return addCommissionRateService.execute(commissionRate)
     }
 }
